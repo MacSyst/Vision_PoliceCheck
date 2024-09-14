48fab3b142eddbc7f9235287214e4a5810972fca
Vision-Scripts <167629435+MacSyst@users.noreply.github.com>
2024-09-14 21:34:37 +0200

    Add files via upload

---
+++ vrp_policecheck/config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+
+-- Job and Command
+Config.jobname = 'police'                        -- Police Job Name
+Config.command = 'funk'                          -- Command Name
+
+-- Keys
+Config.acceptreport = 38                         -- Accept Report   https://docs.fivem.net/docs/game-references/controls/
+Config.rejectreport = 44                        -- Decline Report  https://docs.fivem.net/docs/game-references/controls/
+
+-- Key Names
+Config.keyaccept = "~INPUT_CONTEXT~"             -- Key to accept https://docs.fivem.net/docs/game-references/controls/
+Config.keyreject = "~INPUT_COVER~"              -- Key to decline https://docs.fivem.net/docs/game-references/controls/
+
+-- Main
+Config.debug = true
+Config.delay = 1000
+
+
