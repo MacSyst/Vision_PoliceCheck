bf1dc1ae66c69b5821f4af73371e0c24ef9d735d
Vision-Service <167629435+MacSyst@users.noreply.github.com>
2024-09-20 22:07:25 +0200

    Delete vrp_policecheck directory

--- vrp_policecheck/server/server.lua
+++
@@ -1,9 +0,0 @@
-RegisterServerEvent('vision-police:sendFunk')
-AddEventHandler('vision-police:sendFunk', function(coords)
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-
-    if xPlayer.job.name == Config.jobname then
-        TriggerClientEvent('vision-police:receiveFunk', -1, coords, xPlayer.getName())
-    end
-end)
